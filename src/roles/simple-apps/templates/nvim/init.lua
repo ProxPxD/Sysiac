1522a0c2ba5133054ebe8626e8bb5117b152929c
ProxPxD <piotr10tutek@poczta.onet.pl>
2026-08-01 00:31:12 +0200

    vim

--- src/roles/simple-apps/templates/nvim/init.lua
+++ src/roles/simple-apps/templates/nvim/init.lua
@@ -5,9 +5,19 @@ keymap({ "n", "v" }, "<Down>", "gj")
 keymap({ "n", "v" }, "<Up>", "gk")
 keymap("i", "<Down>", "<C-o>gj")
 keymap("i", "<Up>", "<C-o>gk")
+-- Line displayal
 vim.opt.whichwrap:append("<,>,[,]")
+vim.opt.wrap = true
+vim.opt.linebreak = true
+vim.opt.breakindent = true
+
+-- Tabs
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
 
 -- Word movement
-keymap("n", "<C-Right>", "e")
-keymap("i", "<C-Right>", "<C-o>e")
+keymap("n", "<C-Right>", "e<Right>")
+keymap("i", "<C-Right>", "<C-o>e<Right>")
 
--- src/roles/simple-apps/templates/vim/.vimrc
+++ src/roles/simple-apps/templates/vim/.vimrc
@@ -1,4 +1,16 @@
+# Line break movement
 nnoremap <Down> gj
 nnoremap <Up> gk
 vnoremap <Down> gj
 vnoremap <Up> gk
+
+# Visual Line Breaks
+set wrap
+set linebreak
+set breakindent
+
+# Tabs
+set tabstop=4
+set shiftwidth=4
+set softtabstop=4
+set expandtab
