42f962c8797c61555117106d99915f989685aa18
ProxPxD <piotr10tutek@poczta.onet.pl>
2026-09-03 17:58:28 +0200

    add lsd watch

--- src/roles/fish/files/conf.d/abbr/watch.fish
+++ src/roles/fish/files/conf.d/abbr/watch.fish
@@ -4,3 +4,7 @@ abbr -a -- w1    'watch -n 1 -c'
 abbr -a -- wpps  'watch -n1 -c podman ps -a'
 abbr -a -- wp    'watch -n1 -c podman ps -a'
 abbr -a -- wpl   'watch -n1 -c podman logs'
+## lsd
+abbr -a -- wl    'watch -n1 -c lsd'
+abbr -a -- wls   'watch -n1 -c lsd'
+abbr -a -- wlsd  'watch -n1 -c lsd'
