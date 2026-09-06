04dd944345818a262b05cea5f5e942f236749c53
ProxPxD <piotr10tutek@poczta.onet.pl>
2026-09-06 19:40:06 +0200

    rename

--- src/roles/fish/files/conf.d/abbr/ansible.fish
+++ src/roles/fish/files/conf.d/abbr/ansible.fish
@@ -1,3 +1,3 @@
 ## specific
-abbr -a -- anfconf  'z Sysiac; ansible-playbook  -K src/playbooks/workstation.yml -t confd'
+abbr -a -- anabbr  'z Sysiac; ansible-playbook  -K src/playbooks/workstation.yml -t confd'
 
