7edc9ee1067f6f53daa5c68d6c984aa5301d5327
ProxPxD <piotr10tutek@poczta.onet.pl>
2026-09-07 20:37:45 +0200

    fix mistakes

--- src/roles/fish/files/conf.d/aliases/shutdown.fish
+++ src/roles/fish/files/conf.d/aliases/shutdown.fish
@@ -4,4 +4,4 @@ alias papa    'pa'
 alias siya    'fish -c "sleep 0.2; qdbus6 org.kde.Shutdown /Shutdown logoutAndReboot" &; disown; exit'
 alias nara    'siya'
 alias gudni   'fish -c "sleep 0.2; qdbus6 org.kde.Shutdown /Shutdown logoutAndHibernate" &; disown; exit'
-alias branoc  'gudni
+alias branoc  'gudni'
