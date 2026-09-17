f6855a0d6bd1f7e41341a0e751f45465d75dc28a
ProxPxD <piotr10tutek@poczta.onet.pl>
2026-09-17 15:04:15 +0200

    updt file preserving

--- src/preserve_curr_files.bash
+++ src/preserve_curr_files.bash
@@ -1,1 +1,4 @@
- cp -r  -- ~/.config/fcitx5     ~/Programs/Sysiac/src/roles/fcitx/files/fcitx5/conf
+cp $__fish_config_dir/config.fish ~/Programs/Sysiac/src/roles/shell/files
+cp -r ~/Documents/Obsidian/ProxPxD/.obsidian ~/Programs/Sysiac/src/roles/obsidian/files/.obsidian
+cp ~/.config/kglobalshortcutsrc ~/Programs/Sysiac/src/roles/shortcuts/files
+cp -r ~/.config/fcitx5 ~/Programs/Sysiac/src/roles/fcitx/files/fcitx5/conf
