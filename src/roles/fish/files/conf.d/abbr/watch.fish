e9517b9674f86d4de2023c3c2ef301b21bd5f898
ProxPxD <piotr10tutek@poczta.onet.pl>
2026-09-22 11:17:37 +0200

    Allow colours for watch

--- src/roles/fish/files/conf.d/abbr/watch.fish
+++ src/roles/fish/files/conf.d/abbr/watch.fish
@@ -1,10 +1,10 @@
 ## general
-abbr -a -- w1    'hwatch -n 1 -p'
+abbr -a -- w1    'hwatch -cn 1 -p'
 ## podman
-abbr -a -- wpps  'hwatch -n1 -p podman ps -a'
-abbr -a -- wp    'hwatch -n1 -p podman ps -a'
+abbr -a -- wpps  'hwatch -cn1 -p podman ps -a'
+abbr -a -- wp    'hwatch -ccn1 -p podman ps -a'
 abbr -a -- wpl   'hwatch -n1 -p podman logs'
 ## lsd
-abbr -a -- wl    'hwatch -n1 -p lsd'
-abbr -a -- wls   'hwatch -n1 -p lsd'
-abbr -a -- wlsd  'hwatch -n1 -p lsd'
+abbr -a -- wl    'hwatch -cn1 -p lsd'
+abbr -a -- wls   'hwatch -cn1 -p lsd'
+abbr -a -- wlsd  'hwatch -cn1 -p lsd'
