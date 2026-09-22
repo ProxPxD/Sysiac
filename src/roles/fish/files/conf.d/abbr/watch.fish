f5ef87ad127ad8679c1f1846c2f7e923faaeb35c
ProxPxD <piotr10tutek@poczta.onet.pl>
2026-09-22 13:13:04 +0200

    abbr

--- src/roles/fish/files/conf.d/abbr/watch.fish
+++ src/roles/fish/files/conf.d/abbr/watch.fish
@@ -1,10 +1,10 @@
 ## general
-abbr -a -- w1    'hwatch -cn 1 -p'
+abbr -a -- w1    'hwatch -ctn 1 -p'
 ## podman
-abbr -a -- wpps  'hwatch -cn1 -p podman ps -a'
-abbr -a -- wp    'hwatch -ccn1 -p podman ps -a'
-abbr -a -- wpl   'hwatch -n1 -p podman logs'
+abbr -a -- wpps  'hwatch -ctn1 -p podman ps -a'
+abbr -a -- wp    'hwatch -ctn1 -p podman ps -a'
+abbr -a -- wpl   'hwatch -ctn1 -p podman logs'
 ## lsd
-abbr -a -- wl    'hwatch -cn1 -p lsd'
-abbr -a -- wls   'hwatch -cn1 -p lsd'
-abbr -a -- wlsd  'hwatch -cn1 -p lsd'
+abbr -a -- wl    'hwatch -ctn1 -p lsd'
+abbr -a -- wls   'hwatch -ctn1 -p lsd'
+abbr -a -- wlsd  'hwatch -ctn1 -p lsd'
