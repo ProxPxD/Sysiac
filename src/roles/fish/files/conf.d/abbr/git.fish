486fce14daa35445211029d41f4d3145c6c9a17d
ProxPxD <piotr10tutek@poczta.onet.pl>
2026-09-24 14:38:39 +0200

    add abbr

--- src/roles/fish/files/conf.d/abbr/git.fish
+++ src/roles/fish/files/conf.d/abbr/git.fish
@@ -14,5 +14,6 @@ abbr -a -- gst    'git switch -t'
 abbr -a -- gu     'git push'
 abbr -a -- gd     'git pull'
 abbr -a -- gdu    'git pull; git push'
+abbr -a -- gud    'git pull; git push'
 ## specific
 abbr -a -- gabbr  "z Sysiac; git commit -am 'abbr'; git push; ansible-playbook -K src/playbooks/workstation.yml -t confd; exec fish"
