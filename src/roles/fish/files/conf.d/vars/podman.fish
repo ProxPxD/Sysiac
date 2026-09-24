94631ad0d167a01206d00aa2ffb99f69bc5051f2
ProxPxD <piotr10tutek@poczta.onet.pl>
2026-09-24 16:31:44 +0200

    add unsuported version

--- src/roles/fish/files/conf.d/vars/podman.fish
+++ src/roles/fish/files/conf.d/vars/podman.fish
@@ -1,1 +1,3 @@
 set PODMAN_FORMATTING "table {{.Names}}\t{{.Status}}\t{{.CreatedHuman}}\t{{.Ports}}"
+# Unsupported
+# set PODMAN_FORMATTING "table {{.Names}}\t{{.Label \"app\"}}\t{{.Status}}\t{{.CreatedHuman}}\t{{.Ports}}"
