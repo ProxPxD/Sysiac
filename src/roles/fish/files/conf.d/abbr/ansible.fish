7c271528c9b89ae32b1bf319c01cd4f0c7d18818
ProxPxD <piotr10tutek@poczta.onet.pl>
2026-09-25 16:29:32 +0200

    add abbr

--- src/roles/fish/files/conf.d/abbr/ansible.fish
+++ src/roles/fish/files/conf.d/abbr/ansible.fish
@@ -8,5 +8,6 @@ abbr -a -- anvau    'ansible-vault'
 abbr -a -- anva     'ansible-vault'
 abbr -a -- anv      'ansible-vault'
 abbr -a -- anve     'ansible-vault edit'
+abbr -a -- anvv     'ansible-vault view'
 ## specific
 abbr -a -- anabbr   'z Sysiac; ansible-playbook -K --vault-password-file ~/.config/ansible/vault_password src/playbooks/workstation.yml -t confd; exec fish'
